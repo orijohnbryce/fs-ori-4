4fc9f85b1fe28274edba8647b80de16c0beffa71
Ori Brook <ori@gmail.com>
2025-02-06 19:23:21 +0200

    ex1-solved

---
+++ script.js
@@ -0,0 +1,7 @@
+// let newElement = document.createElement('h1');
+// newElement.innerText = "This is a new h1 element";
+
+// document.querySelector('span').appendChild(newElement);
+
+let text = document.querySelector('#text-input').value;
+console.log(text);
