d471371b28caf169dfea910d20456f9c77396e34
Ori Brook <ori@gmail.com>
2025-02-06 21:10:43 +0200

    continue

---
+++ env.js
@@ -0,0 +1,15 @@
+// document.body.innerHTML = "<p> Hello </p>";
+function myFunc() {    
+    const inputValue = document.querySelector('#input-id').value;
+    
+
+    const results = inputValue * inputValue;
+    document.querySelector('#res-div').innerHTML = results;
+
+}
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-// let newElement = document.createElement('h1');
-// newElement.innerText = "This is a new h1 element";
+let newElement = document.createElement('h1');
+newElement.innerText = "This is a new h1 element";
 
-// document.querySelector('span').appendChild(newElement);
+// document.getElementById('my-span').insertAdjacentElement('afterend', newElement);
+// document.getElementById('my-span').append(newElement);
+// document.getElementById('my-span').before(newElement);
+// document.getElementById('my-span').prepend(newElement);
+// document.getElementById('my-span').after(newElement);
+// document.getElementById('my-span').replaceWith(newElement);
 
-let text = document.querySelector('#text-input').value;
-console.log(text);
+
+
+
+let myCss = document.styleSheets[0].insertRule("#my-p: {color: green;}", 0);
+myCss.insertRule("my-p: {color: green;}", 0);
+
+// document.querySelector('#p-id').classList
+
+
+
