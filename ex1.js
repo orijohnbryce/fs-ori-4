af638eb70260df008bda9eec6b06d5fbbc283d83
Ori Brook <ori@gmail.com>
2025-02-24 18:40:41 +0200

    init branch

---
+++ ex1.js
@@ -0,0 +1,10 @@
+let i = 0;
+
+while (i <= 200){
+
+    // if (i % 17 === 0)
+    if (!(i % 17)) console.log(i);    
+
+    i++;
+}
+
---
+++ script.js
@@ -0,0 +1,47 @@
+
+// function add(x, y) {        
+//     // return x + y;
+
+//     return; // will return undefine 
+// }
+
+// let res = add(4, 50);
+// // let res = add("4", "50");
+// console.log(res);
+
+
+// function tzachi(x) {
+//     x += 10;
+//     return x;    
+// }
+
+// let x = 5;
+// x = tzachi(x);
+
+
+// ********************
+
+// const myfunc = (x, y, z) => {
+//     console.log(x);
+//     x = y + z;
+//     return x;
+// }
+
+// let x = myfunc(5, 1, 2);
+// console.log(x);
+
+// ==========================
+
+// let MY_X = 5;
+
+// function myfunc() {
+//     console.log(MY_X);    
+// }
+// myfunc()
+
+// console.log(global.Math);  // window on browser = global in nodeJS
+
+
+
+
+
